d0bc13202ee5c38a5a6c5be9b9f852e86983ada4
mingus0307 <soeren.krumm@stud.uni-hannover.de>
2025-10-16 13:34:50 +0200

    aufgabe 2

--- prog3/blatt_3/matrix_elem_sum.c
+++ prog3/blatt_3/matrix_elem_sum.c
@@ -1,12 +1,27 @@
 #include <stdio.h>
 
 double matrix_elem_sum(double matrix[], int n, int lengths[]) {
-  
+  double sum = 0; 
+  int num_count = 1; 
+
+  //finde die Anzahl der Einträge  
+  for (int i = 0; i < n; i++){
+    num_count *= lengths[i]; 
+  }
+  //Iteriere durch die Matrix anhand der Anzahl der Einträge 
+  for (int i = 0; i < num_count; i++){
+    sum += matrix[i]; 
+  }
+  //gib die Summe zurück 
+  return sum; 
 }
 
 int main(void) {
   double matrix2d[3][2] = 
-    { {111, 112}, {121, 122}, {131, 132} };
+    { {111, 112}, 
+      {121, 122}, 
+      {131, 132} 
+    };
   int lengths2d[2] = {3, 2};
 
   double matrix3d[2][3][4] = {
