5767dc50d457768134484be791397bb9202271fa
mingus0307 <soeren.krumm@stud.uni-hannover.de>
2025-10-29 14:22:35 +0100

    bsn VL5

--- prog3/blatt_3/aufgabe7/main.c
+++ prog3/blatt_3/aufgabe7/main.c
@@ -3,8 +3,8 @@ int main(void) {
     int i; 
     int* ip; 
     printf("Eingabe: "); 
-    scanf("%d", &i); 
     ip = &i; // & verwandet alles in die Adresse wo es im Speicher steht 
+    scanf("%d", 0x12345678); // ip); // &i); 
     printf("Eingabe war %d\n", 
            *ip ); // * macht so ziemlich das Gegenteil von & also sagt uns was an einer Adresse drin steht im Speicher 
     return 0; 
