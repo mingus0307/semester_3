5a16b317344d22898a91130412221b8d91481da8
mingus0307 <soeren.krumm@stud.uni-hannover.de>
2025-10-30 12:14:41 +0100

    aufgabe 2 ferig

--- prog3/blatt_5/angest.c
+++ prog3/blatt_5/angest.c
@@ -21,41 +21,51 @@ int main(void){
         *pt = NULL; 
     }
 
-    int eingabe =  -1 ;
 
-    while(eingabe == -1 ){
-        printf("bitte Nummer angeben (index zwischen 0-9): \n");  
-        scanf("%d", &eingabe);
-        if(eingabe >= 9 || eingabe < 0){
-            eingabe = -1; 
-        } 
+    char input = 'y'; 
+
+    while (input == 'y'){
+        int eingabe =  -1 ;
+
+        while(eingabe == -1 ){
+            printf("bitte Nummer angeben (index zwischen 0-9): \n");  
+            scanf("%d", &eingabe);
+            if(eingabe >= 9 || eingabe < 0){
+                eingabe = -1; 
+            } 
+        }
+        // wenn array an der stelle leer -> neuen angestellten erstellen 
+        if(arr[eingabe] == NULL){
+            arr[eingabe] = (angestellter*) malloc(sizeof(angestellter)); 
+
+            printf("Neuer Angestellter?\nName eingeben: \n"); 
+            scanf("  %40[^\n]", arr[eingabe]->name);
+
+            printf("Personalnummer eingeben: \n"); 
+            scanf("  %d", &arr[eingabe]->personalnummer);
+
+            printf("Gehalt eingeben: \n"); 
+            scanf("  %f", &arr[eingabe]->gehalt);
+
+        }else{ // sonst löschen wir das ganze raus und machen den Speicher, den es belegt. vorsichtshalber nullen, weil wer weiß       
+            free(arr[eingabe]); 
+            arr[eingabe] = NULL; 
+        }
+
+        printf("\nAlle Angestellten: \n"); 
+        for (int i = 0; i < ARRAY_LEN; i++){
+            if(arr[i] == NULL){
+                continue; 
+            }
+            printf("Name: \n %s\n", arr[i]->name);
+            printf("Personalnummer: \n %d\n", arr[i]->personalnummer);
+            printf("Gehalt: \n %f\n", arr[i]->gehalt); 
+            printf("\n"); 
+        }
+
+        printf("weitermachen? y oder n\n"); 
+        scanf(" %1c", &input);
     }
-    // wenn array an der stelle leer -> neuen angestellten erstellen 
-    if(arr[eingabe] == NULL){
-        arr[eingabe] = (angestellter*) malloc(sizeof(angestellter)); 
-
-        printf("Neuer Angestellter?\n Name eingeben: \n"); 
-        scanf("%40s", arr[eingabe]->name);
-
-        printf("Personalnummer eingeben: \n"); 
-        scanf("%d", &arr[eingabe]->personalnummer);
-
-        printf("Gehalt eingeben: \n"); 
-        scanf("%f", &arr[eingabe]->gehalt);
-
-    }else{ // sonst löschen wir das ganze raus und machen den Speicher, den es belegt. vorsichtshalber nullen, weil wer weiß       
-        free(arr[eingabe]); 
-        arr[eingabe] = NULL; 
-    }
-
-    printf("Alle Angestellten: \n"); 
-    for (int i = 0; i < ARRAY_LEN; i++){
-        printf("Name: \n %s", arr[i]->name);
-        printf("Personalnummer: \n %d", arr[i]->personalnummer);
-        printf("Gehalt: \n %f", arr[i]->gehalt); 
-        printf("\n"); 
-    }
-
 
 
 
