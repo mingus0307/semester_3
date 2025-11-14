f2fdd63305f5fe8cba789bd6907af8528dd3d8d8
Sören <122535094+mingus0307@users.noreply.github.com>
2025-11-14 16:05:51 +0100

    sql part for the second part of the bonus

---
+++ dbs2/bonus_abgabe_teil2.sql
@@ -0,0 +1,18 @@
+
+INSERT INTO person (person_id, name) VALUES (nextval('person_id_seq'), 'Brad Pitt');
+INSERT INTO person (person_id, name) VALUES (nextval('person_id_seq'), 'Morgan Freeman');
+INSERT INTO person (person_id, name) VALUES (nextval('person_id_seq'), 'Natalie Portman');
+INSERT INTO person (person_id, name) VALUES (nextval('person_id_seq'), 'Denzel Washington');
+INSERT INTO person (person_id, name) VALUES (nextval('person_id_seq'), 'Scarlett Johansson');
+INSERT INTO person (person_id, name) VALUES (nextval('person_id_seq'), 'Christian Bale');
+
+
+
+INSERT INTO Genre (genre_id, genre) VALUES (nextval('genre_id_seq'), 'Thriller')
+INSERT INTO Genre (genre_id, genre) VALUES (nextval('genre_id_seq'), 'Drama');
+INSERT INTO Genre (genre_id, genre) VALUES (nextval('genre_id_seq'), 'Science Fiction');
+INSERT INTO Genre (genre_id, genre) VALUES (nextval('genre_id_seq'), 'Action');
+INSERT INTO Genre (genre_id, genre) VALUES (nextval('genre_id_seq'), 'Fantasy');
+INSERT INTO Genre (genre_id, genre) VALUES (nextval('genre_id_seq'), 'Horror');
+
+
