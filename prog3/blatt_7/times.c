9c8cbde0c5e173cf3cfa331aff7ce44bbaa5e06f
mingus0307 <soeren.krumm@stud.uni-hannover.de>
2025-11-25 14:59:13 +0100

    blatt 7 aufgabe 1

--- prog3/blatt_7/times.c
+++ prog3/blatt_7/times.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
 
+
+void print(void) { printf("Hallo\n"); }
+
+void times(int  n, void (*fn)(void)){
+    for (int i = 0; i < n; i++){
+        fn(); 
+    }
+} 
+
 int main(void){
+    int foo = 3; 
+    void (*fn)(void) = print; 
+
+    times(foo, print); 
+    
     return 0; 
 }
