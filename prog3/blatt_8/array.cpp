7b83387350f2eab0c044e718af2db9a707f8bbd5
mingus0307 <soeren.krumm@stud.uni-hannover.de>
2025-12-03 23:12:01 +0100

    prog3 blatt_8 aufgabe 1

---
+++ prog3/blatt_8/array.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int* createArray(int anz){
+    int* fin= new int[anz](); 
+
+    return fin; 
+}
+
+void releaseArray(int*& a){
+    delete[] a; 
+    a = nullptr; 
+}
+
+int main(void){
+    int* a = createArray(5); 
+    a[2] = 42; 
+    std::cout<<a[2]<< '\n'; 
+
+    releaseArray(a);
+    std::cout<<(a == nullptr)<< '\n';  
+}
